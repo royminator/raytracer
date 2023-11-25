4cd03f7a4240cf24cf0855b3d69beab6b5615012
Roy Angelsen <roy.angelsen@gmail.com>
2023-11-25 22:55:32 +0100

    feat: V4 equality

--- test/DomainTests.hs
+++ test/DomainTests.hs
@@ -13,6 +13,7 @@ domainTests =
     , vectorWShouldBe0
     , floatEqualityShouldBeTrue
     , floatEqualityShouldBeFalse
+    , v4EqualityShouldBeEqual
     ]
 
 pointWShouldBe1 :: TestTree
@@ -25,8 +26,15 @@ vectorWShouldBe0 =
 
 floatEqualityShouldBeTrue :: TestTree
 floatEqualityShouldBeTrue =
-  testCase "float should be eq" $ 0.98198 ~= 0.981981 @?= True
+  testCase "float should be eq" $
+  (0.98198 :: Float) ~= (0.981981 :: Float) @?= True
 
 floatEqualityShouldBeFalse :: TestTree
 floatEqualityShouldBeFalse =
-  testCase "float equality be neq" $ 0.001 ~= 0.0011 @?= False
+  testCase "float equality be neq" $
+  (0.001 :: Float) ~= (0.0011 :: Float) @?= False
+
+v4EqualityShouldBeEqual :: TestTree
+v4EqualityShouldBeEqual =
+  testCase "vector equality should be eq" $
+  V4 0.1 0.2 0.3 0.4 ~= V4 0.1 0.2 0.3 0.4 @?= True
