c55a057888a9444916078d0a58433316cbcab1ef
Roy Angelsen <roy.angelsen@gmail.com>
2023-11-26 15:17:12 +0100

    test: vector subtraction

--- test/MathTests.hs
+++ test/MathTests.hs
@@ -16,6 +16,10 @@ mathTests =
     , v4EqualityShouldBeTrue
     , v4EqualityShouldBeFalse
     , v4AdditionShouldBeCorrect
+    , subtractTwoPointsShouldBeCorrect 
+    , subtractVectorFromPoint
+    , subtractTwoVectors
+    , subtractVectorFromZeroVector
     ]
 
 pointWShouldBe1 :: TestTree
@@ -49,4 +53,24 @@ v4EqualityShouldBeFalse =
 v4AdditionShouldBeCorrect :: TestTree
 v4AdditionShouldBeCorrect =
   testCase "vector addition should be correct" $
-  V4 1 2 3 4 + V4 5 6 7 8 @?= V4 6 8 10 12
+  V4 3 (-2) 5 1 + V4 (-2) 3 1 0 @?= V4 1 1 6 1
+
+subtractTwoPointsShouldBeCorrect :: TestTree
+subtractTwoPointsShouldBeCorrect =
+  testCase "vector subtraction should be correct" $
+  point 3 2 1 - point 5 6 7 @?= vector (-2) (-4) (-6)
+
+subtractVectorFromPoint :: TestTree
+subtractVectorFromPoint =
+  testCase "subtract vector from point" $
+  point 3 2 1 - vector 5 6 7 @?= point (-2) (-4) (-6)
+
+subtractTwoVectors :: TestTree
+subtractTwoVectors =
+  testCase "subtract two vectors" $
+  vector 3 2 1 - vector 5 6 7 @?= vector (-2) (-4) (-6)
+
+subtractVectorFromZeroVector :: TestTree
+subtractVectorFromZeroVector =
+  testCase "subtract vector from zero vector" $
+  (0 :: V4) - vector 1 (-2) 3 @?= vector (-1) 2 (-3)
