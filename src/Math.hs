2d10d7771c6570b9aadffbb949f4d34172adbe07
Roy Angelsen <roy.angelsen@gmail.com>
2024-02-24 13:57:43 +0100

    feat: avoid shadowing

--- src/Math.hs
+++ src/Math.hs
@@ -3,11 +3,16 @@ module Math (module Math) where
 class ApproxEq a where
   (~=) :: a -> a -> Bool
 
-data V4 = V4 Float Float Float Float
+data V4 = V4
+  { x :: Float,
+    y :: Float,
+    z :: Float,
+    w :: Float
+  }
   deriving (Eq, Show)
 
 instance ApproxEq Float where
-  (~=) x y = abs (x - y) < eps
+  (~=) a b = abs (a - b) < eps
     where
       eps = 1e-5
 
@@ -18,21 +23,20 @@ instance Num V4 where
     V4 (x1 - x2) (y1 - y2) (z1 - z2) (w1 - w2)
   (*) (V4 x1 y1 z1 w1) (V4 x2 y2 z2 w2) =
     V4 (x1 * x2) (y1 * y2) (z1 * z2) (w1 * w2)
-  negate (V4 x y z w) = V4 (-x) (-y) (-z) (-w)
-  abs (V4 x y z w) = V4 (abs x) (abs y) (abs z) (abs w)
-  signum (V4 x y z w) = V4 (signum x) (signum y) (signum z) (signum w)
-  fromInteger x = V4 (fromInteger x) (fromInteger x) (fromInteger x) (fromInteger x)
+  negate (V4 x_ y_ z_ w_) = V4 (-x_) (-y_) (-z_) (-w_)
+  abs (V4 x_ y_ z_ w_) = V4 (abs x_) (abs y_) (abs z_) (abs w_)
+  signum (V4 x_ y_ z_ w_) = V4 (signum x_) (signum y_) (signum z_) (signum w_)
+  fromInteger x_ = V4 (fromInteger x_) (fromInteger x_) (fromInteger x_) (fromInteger x_)
 
 instance ApproxEq V4 where
   (~=) (V4 x1 y1 z1 w1) (V4 x2 y2 z2 w2) =
-    x1 ~= x2 &&
-    y1 ~= y2 &&
-    z1 ~= z2 &&
-    w1 ~= w2
+    x1 ~= x2
+      && y1 ~= y2
+      && z1 ~= z2
+      && w1 ~= w2
 
 point :: Float -> Float -> Float -> V4
-point x y z = V4 x y z 1
+point x_ y_ z_ = V4 x_ y_ z_ 1
 
 vector :: Float -> Float -> Float -> V4
-vector x y z = V4 x y z 0
-
+vector x_ y_ z_ = V4 x_ y_ z_ 0
--- test/MathTests.hs
+++ test/MathTests.hs
@@ -6,71 +6,28 @@ import Test.Tasty.HUnit (testCase, (@?=))
 import Math
 
 mathTests :: TestTree
-mathTests =
-  testGroup
-    "Math Tests"
-    [ pointWShouldBe1
-    , vectorWShouldBe0
-    , floatEqualityShouldBeTrue
-    , floatEqualityShouldBeFalse
-    , v4EqualityShouldBeTrue
-    , v4EqualityShouldBeFalse
-    , v4AdditionShouldBeCorrect
-    , subtractTwoPointsShouldBeCorrect 
-    , subtractVectorFromPoint
-    , subtractTwoVectors
-    , subtractVectorFromZeroVector
-    ]
-
-pointWShouldBe1 :: TestTree
-pointWShouldBe1 =
-  testCase "point w = 1" $ V4 1 2 3 1 @?= point 1 2 3
-
-vectorWShouldBe0 :: TestTree
-vectorWShouldBe0 =
-  testCase "vector w = 0" $ V4 1 2 3 0 @?= vector 1 2 3
-
-floatEqualityShouldBeTrue :: TestTree
-floatEqualityShouldBeTrue =
-  testCase "float should be eq" $
-  (0.98198 :: Float) ~= (0.981981 :: Float) @?= True
-
-floatEqualityShouldBeFalse :: TestTree
-floatEqualityShouldBeFalse =
-  testCase "float equality be neq" $
-  (0.001 :: Float) ~= (0.0011 :: Float) @?= False
-
-v4EqualityShouldBeTrue :: TestTree
-v4EqualityShouldBeTrue =
-  testCase "vector equality should be eq" $
-  V4 0.1 0.2 0.3 0.4 ~= V4 0.1 0.2 0.3 0.4 @?= True
-
-v4EqualityShouldBeFalse :: TestTree
-v4EqualityShouldBeFalse =
-  testCase "vector equality should be neq" $
-  V4 0.1 0.2 0.3 0.4 ~= V4 0.1 0.2 0.30003 0.4 @?= False
-
-v4AdditionShouldBeCorrect :: TestTree
-v4AdditionShouldBeCorrect =
-  testCase "vector addition should be correct" $
-  V4 3 (-2) 5 1 + V4 (-2) 3 1 0 @?= V4 1 1 6 1
-
-subtractTwoPointsShouldBeCorrect :: TestTree
-subtractTwoPointsShouldBeCorrect =
-  testCase "vector subtraction should be correct" $
-  point 3 2 1 - point 5 6 7 @?= vector (-2) (-4) (-6)
-
-subtractVectorFromPoint :: TestTree
-subtractVectorFromPoint =
-  testCase "subtract vector from point" $
-  point 3 2 1 - vector 5 6 7 @?= point (-2) (-4) (-6)
-
-subtractTwoVectors :: TestTree
-subtractTwoVectors =
-  testCase "subtract two vectors" $
-  vector 3 2 1 - vector 5 6 7 @?= vector (-2) (-4) (-6)
-
-subtractVectorFromZeroVector :: TestTree
-subtractVectorFromZeroVector =
-  testCase "subtract vector from zero vector" $
-  (0 :: V4) - vector 1 (-2) 3 @?= vector (-1) 2 (-3)
+mathTests = testGroup
+  "Math Tests"
+  [ testCase "point w = 1" $
+      V4 1 2 3 1 @?= point 1 2 3
+  , testCase "vector w = 0" $
+      V4 1 2 3 0 @?= vector 1 2 3
+  , testCase "float should be eq" $
+      (0.98198 :: Float) ~= (0.981981 :: Float) @?= True
+  , testCase "float equality be neq" $
+      (0.001 :: Float) ~= (0.0011 :: Float) @?= False
+  , testCase "vector equality should be eq" $
+      V4 0.1 0.2 0.3 0.4 ~= V4 0.1 0.2 0.3 0.4 @?= True
+  , testCase "vector equality should be neq" $
+      V4 0.1 0.2 0.3 0.4 ~= V4 0.1 0.2 0.30003 0.4 @?= False
+  , testCase "vector addition should be correct" $
+      V4 3 (-2) 5 1 + V4 (-2) 3 1 0 @?= V4 1 1 6 1
+  , testCase "vector subtraction should be correct" $
+      point 3 2 1 - point 5 6 7 @?= vector (-2) (-4) (-6)
+  , testCase "subtract vector from point" $
+      point 3 2 1 - vector 5 6 7 @?= point (-2) (-4) (-6)
+  , testCase "subtract two vectors" $
+      vector 3 2 1 - vector 5 6 7 @?= vector (-2) (-4) (-6)
+  , testCase "subtract vector from zero vector" $
+      (0 :: V4) - vector 1 (-2) 3 @?= vector (-1) 2 (-3)
+  ]
